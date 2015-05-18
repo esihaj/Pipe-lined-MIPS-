b1b517b41a621238ad1aba3a5329d56dcbc63acb
amirmasud <zare.masud@gmail.com>
2015-05-19 00:56:47 +0430

    Fixed: ID_EX input output mismatch

--- ID_EX.v
+++ ID_EX.v
@@ -8,8 +8,8 @@ module ID_EX (input clk, reset,
 	/*controller*/
 	output ID_EX_mem_write, ID_EX_reg_write, ID_EX_alu_use_carry, ID_EX_alu_in_mux, ID_EX_select_c, ID_EX_select_z, ID_EX_write_c, ID_EX_write_z, output [2:0] ID_EX_alu_op, output [1:0] ID_EX_reg_write_mux);
 
-	M_S_FF #(8) reg_A(clk, reset, reg_in_A, reg_out_A);
-	M_S_FF #(8) reg_B(clk, reset, reg_in_B, reg_out_B);
+	M_S_FF #(8) reg_A(clk, reset, reg_in_A, ID_EX_A);
+	M_S_FF #(8) reg_B(clk, reset, reg_in_B, ID_EX_B);
 	M_S_FF #(19) instruction(clk, reset, IF_ID_instruction, ID_EX_instruction);
 	
 	M_S_FF #(13) controller(clk, reset,
