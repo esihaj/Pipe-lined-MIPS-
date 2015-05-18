3d839e0bb00d4ddc2e87dbbaefaf7991cc6d75c9
esihaj <esihaj@gmail.com>
2015-05-18 22:18:23 +0430

    FIXED: forgot to pass [18:0] instruction along

--- MEM_WB.v
+++ MEM_WB.v
@@ -1,7 +1,10 @@
-module MEM_WB(input clk, reset, input [7:0] MEM_WB_mem_out_data, EX_MEM_alu_out, EX_MEM_shift_out, input EX_MEM_reg_write_mux,
-							output [7:0] MEM_WB_mem_out_data, MEM_WB_alu_out, MEM_WB_shift_out, output MEM_WB_reg_write_mux);
+module MEM_WB(input clk, reset, input [7:0] MEM_WB_mem_out_data, EX_MEM_alu_out, EX_MEM_shift_out, input [18:0] EX_MEM_instruction
+							input [1:0] EX_MEM_reg_write_mux,
+							output [7:0] MEM_WB_mem_out_data, MEM_WB_alu_out, MEM_WB_shift_out, output [18:0] MEM_WB_instruction
+							output MEM_WB_reg_write_mux);
 	
 	M_S_FF #(24) datapath(clk, reset, {MEM_WB_mem_out_data, EX_MEM_alu_out, EX_MEM_shift_out}, {MEM_WB_mem_out_data, MEM_WB_alu_out, MEM_WB_shift_out});
-	M_S_FF #(4) controller(clk, reset, EX_MEM_reg_write_mux, MEM_WB_reg_write_mux);
+	M_S_FF #(19) instruction(clk, reset, EX_MEM_instruction, MEM_WB_instruction);
+	M_S_FF #(2) controller(clk, reset, EX_MEM_reg_write_mux, MEM_WB_reg_write_mux);
 
 endmodule
